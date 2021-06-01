648da37537d5628c7a2185ec7245ea1085ddbd9c
EtMi832 <anzhuiweiping@126.com>
2021-06-01 11:34:58 +0800

    开始第一个想

---
+++ study/day3/JSON.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+"""
+    JSON.py
+Created by PythonStudy on 2017/5/19 下午6:39
+Copyright 2017 azhen All rights reserved.
+"""
+
+import json
+import pickle
+
+
+
+import os
+
+print(os.path.dirname(os.path.abspath(__file__)))
---
+++ study/day3/func_sys.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+"""
+    func_sys.py
+Created by PythonStudy on 2017/5/19 下午3:09
+Copyright 2017 azhen All rights reserved.
+"""
+
+import time
+
+# 判断可迭代对象元素所有的值是否为真
+# all()
+
+# 可迭代对象任意一个对象微针即为真
+# any()
+
+#十进制转换二进制
+# print(bin(4))
+# 判断真假
+# print(bool(4))
+#
+# print(bytearray("dsff"))
+#
+
+print(hash("sdfs"))
